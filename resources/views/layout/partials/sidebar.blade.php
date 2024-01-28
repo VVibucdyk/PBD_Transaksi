355b19d26a8cd26e3fab868a7ece1df2f9bffaa3
Fajar Alam <sidik3998@gmail.com>
2024-01-28 22:15:26 +0700

    ganti sidebar jadi dashboard PBD

--- resources/views/layout/partials/sidebar.blade.php
+++ resources/views/layout/partials/sidebar.blade.php
@@ -14,7 +14,7 @@
 
     <!-- Nav Item - Dashboard -->
     <li class="nav-item">
-        <a class="nav-link" href="{{route("dashboard")}}"><i class="fas fa-fw fa-tachometer-alt"></i><span>Dashboard</span></a>
+        <a class="nav-link" href="{{route("dashboard")}}"><i class="fas fa-fw fa-tachometer-alt"></i><span>Dashboard PBD</span></a>
     </li>
 
     <!-- Heading -->
