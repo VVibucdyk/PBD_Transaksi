b4651652f8260f0ffca8761fcb91e90310bbdf88
JoyMRizal <joymrizal@gmail.com>
2024-02-11 16:46:28 +0700

    perapihan faktur index, bisa mengikuti yang saya yah

--- resources/views/faktur/index.blade.php
+++ resources/views/faktur/index.blade.php
@@ -1,83 +1,92 @@
 @extends('layout.main')
 @section('content-wrapper')
 <div class="container-fluid">
-
-    <!-- Page Heading -->
-    <h1 class="h3 mb-4 text-gray-800">TABEL FAKTUR</h1>
-    <div class="pb-3">
-        <form class="d-flex" action="{{ url('faktur') }}" method="get">
-            <input class="form-control me-1" type="search" name="katakunci" value="{{ Request::get('katakunci') }}" placeholder="Masukkan Kode_Faktur/No_Faktur_Pajak/No_Surat_Jalan/No_Surat_Pembelian/Tanggal_Faktur/Kode_Pemesan" aria-label="Search">
-            <button class="btn btn-secondary" type="submit">Cari</button>
-        </form>
+    <div class="card shadow mb-4">
+        <div class="card-body">
+            <!-- FORM PENCARIAN & TAMBAH DATA-->
+            <div class="d-flex justify-content-between mb-3 align-items-center">
+                <h1 class="h3 mb-0 text-gray-800"><strong>Daftar Faktur</strong></h1>
+                <form class="d-inline-block" action="{{ url('faktur') }}" method="get">
+                    <div class="input-group">
+                        <input class="form-control form-control-sm" type="text" name="katakunci" value="{{ Request::get('katakunci') }}" placeholder="Cari Faktur" aria-label="Search">
+                        <div class="input-group-append">
+                            <button class="btn btn-outline-primary btn-sm" type="submit">
+                                <i class="fas fa-search"></i> <!-- FontAwesome Icon -->
+                            </button>
+                            <!-- TOMBOL TAMBAH DATA -->
+                            <a href="{{ url('faktur/create') }}" class="btn btn-primary btn-sm ml-2">
+                                <i class="fas fa-plus"></i> Tambah Faktur Baru
+                            </a>
+                        </div>
+                    </div>
+                </form>
+            </div>
+            <!-- TABEL -->
+            <div class="table-responsive">
+                <table class="table table-striped">
+                    <thead>
+                        <tr>
+                            <th class="">No</th>
+                            <th class="">Faktur</th>
+                            <th class="">Faktur Pajak</th>
+                            <th class="">Surat Jalan</th>
+                            <th class="">Surat Pembelian</th>
+                            <th class="">Tanggal</th>
+                            <th class="">Pemesan</th>
+                            <th class="">Subtotal</th>
+                            <th class="">DP</th>
+                            <th class="">Diskon</th>
+                            <th class="">PPN</th>
+                            <th class="">Total</th>
+                            <th class="">Status</th>
+                            {{-- <th class="col-md-1">Aksi</th> --}}
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php $i = $data->firstItem() ?>
+                        @foreach ($data as $item)
+                        <tr>
+                            <td>{{ $i }}</td>
+                            <td>{{ $item->Kode_Faktur }}</td>
+                            <td>{{ $item->No_Faktur_Pajak }}</td>
+                            <td>{{ $item->No_Surat_Jalan }}</td>
+                            <td>{{ $item->No_Surat_Pembelian }}</td>
+                            <td>{{ $item->Tanggal_Faktur }}</td>
+                            <td>{{ $item->pemesan->Nama_Pemesan }}</td>
+                            <td>{{ "Rp " . number_format($item->Subtotal, 0, ',', '.') }}</td>
+                            <td>{{ $item->DP."%" }}</td>
+                            <td>{{ $item->Diskon_Harga."%" }}</td>
+                            <td>{{ "Rp " . number_format($item->PPN, 0, ',', '.') }}</td>
+                            <td>{{ "Rp " . number_format($item->Total, 0, ',', '.') }}</td>
+                            <td>
+                                <form onsubmit="return confirm('Yakin akan mengganti status FAKTUR?')" class="d-inline" action="{{ url('faktur/'.$item->Kode_Faktur) }}" method="post">
+                                    @csrf
+                                    @method('PUT')
+                                        <button style="width: 80px; type="submit" name="submit" class="btn btn-sm 
+                                    @if ($item->Status == 'Open')
+                                        btn-success"><i class="far fa-eye"></i>
+                                    @else
+                                        btn-danger"><i class="far fa-eye-slash"></i>
+                                    @endif
+                                        {{ $item->Status }}</button>
+                                </form>
+                            </td>
+                            {{-- <td>
+                                <a href='{{ url('faktur/'.$item->Kode_Faktur.'/edit') }}' class="btn btn-warning btn-sm">Edit</a>
+                                <form onsubmit="return confirm('Yakin akan menghapus data?')" class="d-inline" action="{{ url('faktur/'.$item->Kode_Faktur) }}" method="post">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button type="submit" name="submit" class="btn btn-danger btn-sm">Hapus</button>
+                                </form>
+                            </td> --}}
+                        </tr>
+                        <?php $i++ ?> 
+                        @endforeach
+                    </tbody>
+                </table class="table table-striped">
+                {{ $data->withQueryString()->links() }}
+            </div>
+        </div>
     </div>
-
-    <!-- TOMBOL TAMBAH DATA -->
-    <div class="pb-3">
-        <a href='{{ url('faktur/create') }}' class="btn btn-primary">Tambah Data</a>
-    </div>
-
-    <!-- TABEL -->
-    <table class="table table-striped">
-        <thead>
-            <tr>
-                <th class="">No</th>
-                <th class="">Faktur</th>
-                <th class="">Faktur Pajak</th>
-                <th class="">Surat Jalan</th>
-                <th class="">Surat Pembelian</th>
-                <th class="">Tanggal</th>
-                <th class="">Pemesan</th>
-                <th class="">Subtotal</th>
-                <th class="">DP</th>
-                <th class="">Diskon</th>
-                <th class="">PPN</th>
-                <th class="">Total</th>
-                <th class="">Status</th>
-                {{-- <th class="col-md-1">Aksi</th> --}}
-            </tr>
-        </thead>
-        <tbody>
-            <?php $i = $data->firstItem() ?>
-            @foreach ($data as $item)
-            <tr>
-                <td>{{ $i }}</td>
-                <td>{{ $item->Kode_Faktur }}</td>
-                <td>{{ $item->No_Faktur_Pajak }}</td>
-                <td>{{ $item->No_Surat_Jalan }}</td>
-                <td>{{ $item->No_Surat_Pembelian }}</td>
-                <td>{{ $item->Tanggal_Faktur }}</td>
-                <td>{{ $item->pemesan->Nama_Pemesan }}</td>
-                <td>{{ "Rp " . number_format($item->Subtotal, 0, ',', '.') }}</td>
-                <td>{{ $item->DP."%" }}</td>
-                <td>{{ $item->Diskon_Harga."%" }}</td>
-                <td>{{ "Rp " . number_format($item->PPN, 0, ',', '.') }}</td>
-                <td>{{ "Rp " . number_format($item->Total, 0, ',', '.') }}</td>
-                <td>
-                    <form onsubmit="return confirm('Yakin akan mengganti status FAKTUR?')" class="d-inline" action="{{ url('faktur/'.$item->Kode_Faktur) }}" method="post">
-                        @csrf
-                        @method('PUT')
-                        <button type="submit" name="submit"
-                        @if ($item->Status == 'Open')
-                            class="btn btn-success btn-sm">{{ $item->Status }}</button>
-                        @else
-                            class="btn btn-danger btn-sm">{{ $item->Status }}</button>
-                        @endif
-                    </form>
-                </td>
-                <td>
-                    {{-- <a href='{{ url('faktur/'.$item->Kode_Faktur.'/edit') }}' class="btn btn-warning btn-sm">Edit</a> --}}
-                    {{-- <form onsubmit="return confirm('Yakin akan menghapus data?')" class="d-inline" action="{{ url('faktur/'.$item->Kode_Faktur) }}" method="post">
-                        @csrf
-                        @method('DELETE')
-                        <button type="submit" name="submit" class="btn btn-danger btn-sm">Hapus</button>
-                    </form> --}}
-                </td>
-            </tr>
-            <?php $i++ ?> 
-            @endforeach
-        </tbody>
-    </table class="table table-striped">
-    {{ $data->withQueryString()->links() }}
-    <br>
 </div>
 @endsection
